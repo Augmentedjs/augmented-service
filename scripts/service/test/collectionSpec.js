5234edf455b14d338137c9ba262c3c3033843d7b
Bob Warren <jedi.master@sbcglobal.net>
2016-10-07 16:10:33 -0700

    fist split of augmented-service

---
+++ scripts/service/test/collectionSpec.js
@@ -0,0 +1,23 @@
+//define(["augmented", "augmentedService"], function(Augmented) {
+XMLHttpRequest = require("xhr2");
+const Augmented = require("augmentedjs");
+      Augmented.Service = require("../service");
+
+    describe("Given an Augmented Service Collection", function() {
+        it("is defined", function() {
+            expect(Augmented.Service.Collection).toBeDefined();
+        });
+
+        var e;
+        beforeEach(function() {
+            e = new Augmented.Service.Collection();
+        });
+        afterEach(function() {
+            e = null;
+        });
+
+        it("can check if empty", function() {
+            expect(e.isEmpty()).toBeTruthy();
+        });
+    });
+//});
---
+++ scripts/service/test/datasourceSpec.js
@@ -0,0 +1,37 @@
+//define(["augmented", "augmentedService"], function(Augmented) {
+XMLHttpRequest = require("xhr2");
+const Augmented = require("augmentedjs");
+      Augmented.Service = require("../service");
+
+    describe("Given an Augmented Service DataSourceFactory", function() {
+        it("a factory is defined", function() {
+            expect(Augmented.Service.DataSourceFactory).toBeDefined();
+        });
+
+        describe("Given a DataSource instance", function() {
+            it("is defined", function() {
+                expect(Augmented.Service.DataSource).toBeDefined();
+            });
+        });
+
+        describe("Given a MongoDB DataSource", function() {
+            it("can get a MongoDB DataSource instance", function() {
+                var ds = Augmented.Service.DataSourceFactory.getDatasource(
+                    Augmented.Service.DataSourceFactory.Type.MongoDB, {});
+
+                expect(ds).toBeDefined();
+                expect(ds instanceof Augmented.Service.DataSource).toBeTruthy();
+            });
+        });
+
+        describe("Given a SOLR DataSource", function() {
+            it("can get a SOLR DataSource instance", function() {
+                var ds = Augmented.Service.DataSourceFactory.getDatasource(
+                    Augmented.Service.DataSourceFactory.Type.SOLR, {});
+
+                expect(ds).toBeDefined();
+                expect(ds instanceof Augmented.Service.DataSource).toBeTruthy();
+            });
+        });
+    });
+//});
